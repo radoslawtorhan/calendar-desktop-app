319d22919666363895291a66f52da26b90062913
Radoslaw Torhan <radoslaw.torhan@gmail.com>
2023-01-18 16:53:27 +0100

    Initial commit of calendar organizer desktop app

---
+++ CalendarGui.py
@@ -0,0 +1,69 @@
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QApplication
+from user import UserAPI
+
+
+class GUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.user = UserAPI()
+
+        # Create widgets
+        self.calendar = QtWidgets.QCalendarWidget()
+        self.event_list = QtWidgets.QListWidget()
+        self.event_input = QtWidgets.QLineEdit()
+        self.add_event_button = QtWidgets.QPushButton("Add Event")
+        self.user_combo_box = QtWidgets.QComboBox()
+        self.user_combo_box.addItems(self.user.get_all_users())
+        self.add_user_button = QtWidgets.QPushButton("Add User")
+        self.user_input = QtWidgets.QLineEdit()
+
+        # Create layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.calendar)
+        layout.addWidget(self.event_list)
+        layout.addWidget(self.event_input)
+        layout.addWidget(self.add_event_button)
+        layout.addWidget(self.user_combo_box)
+        layout.addWidget(self.user_input)
+        layout.addWidget(self.add_user_button)
+
+        # Set layout
+        self.setLayout(layout)
+
+        # Connect signals
+        self.calendar.selectionChanged.connect(self.display_events)
+        self.add_event_button.clicked.connect(self.add_event)
+        self.user_combo_box.currentIndexChanged.connect(self.update_user)
+        self.add_user_button.clicked.connect(self.add_user)
+
+    def display_events(self):
+        date = self.calendar.selectedDate()
+        events = self.user.get_events(date)
+        self.event_list.clear()
+        self.event_list.addItems(events)
+
+    def add_event(self):
+        event = self.event_input.text()
+        date = self.calendar.selectedDate()
+        self.user.add_event(date, event)
+        self.display_events()
+
+    def add_user(self):
+        name = self.user_input.text()
+        self.user.add_user(name)
+        self.user_combo_box.addItem(name)
+        print('user added')
+
+    def update_user(self):
+        user = self.user_combo_box.currentText()
+        self.user.set_current_user(user)
+        self.display_events()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    calendar_organizer = GUI()
+    calendar_organizer.show()
+    app.exec_()
---
+++ user.py
@@ -0,0 +1,34 @@
+class UserAPI:
+
+    def __init__(self):
+        self.users = {}
+        self.current_user = None
+
+    def add_user(self, name):
+        self.users[name] = {}
+
+    def set_current_user(self, name):
+        if name in self.users:
+            self.current_user = name
+        else:
+            print(f"User {name} does not exist.")
+
+    def get_all_users(self):
+        return list(self.users.keys())
+
+    def add_event(self, date, event):
+        if self.current_user:
+            if date not in self.users[self.current_user]:
+                self.users[self.current_user][date] = []
+            self.users[self.current_user][date].append(event)
+        else:
+            print("No user selected.")
+
+    def get_events(self, date):
+        if self.current_user:
+            if date in self.users[self.current_user]:
+                return self.users[self.current_user][date]
+            else:
+                return []
+        else:
+            print("No user selected.")
